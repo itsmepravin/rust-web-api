057cc1476b997c91e30c83f9d5d2f4ba8092d848
itsmepravin <singh.pravin19@yahoo.com>
2024-01-25 23:03:42 +0545

    implementation of Post Handlers

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,10 +1,11 @@
 use iron::headers::ContentType;
+use iron::status::Status::Ok;
 use iron::{status, AfterMiddleware, Handler, IronResult, Request, Response};
 use rustc_serialize::json;
 use std::io::Read;
 use std::sync::{Arc, Mutex};
 
-use crate::database::Database;
+use crate::database::{self, Database};
 use crate::models::Post;
 use router::Router;
 use std::error::Error;
@@ -48,4 +49,37 @@ macro_rules! get_http_param {
             None => return Ok(Response::with(status::InternalServerError))
         }
     };
+}
+
+pub struct Handlers {
+    pub post_feed: PostFeedHandler,
+    pub post_post: PostPostHandler,
+    pub post: PostHandler,
+}
+
+impl Handlers {
+    pub fn new(db: Database) -> Handlers {
+        let database = Arc::new(Mutex::new(db));
+        Handlers {
+            post_feed: PostFeedHandler::new(database.clone()),
+            post_post: PostPostHandler::new(database.clone()),
+            post: PostHandler::new(database.clone()),
+        }
+    }
+}
+pub struct PostFeedHandler {
+    database: Arc<Mutex<Database>>,
+}
+
+impl PostFeedHandler {
+    fn new(database: Arc<Mutex<Database>>) -> PostFeedHandler {
+        PostFeedHandler { database }
+    }
+}
+
+impl Handler for PostFeedHandler {
+    fn handle(&self, _: &mut Request) -> IronResult<Response> {
+        let payload = try_handler!(json::encode(lock!(self.database).posts()));
+        Ok(Response::with((status::Ok, payload)))
+    }
 }
